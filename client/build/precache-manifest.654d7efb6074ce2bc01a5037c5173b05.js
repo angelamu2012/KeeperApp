7fc3ba1ba4da8bd46798c23ec41e03c0f61e9a88
Angela Mu <angela@Angelas-MBP-4.lan>
2023-12-09 22:13:12 -0500

    fix react-scripts

---
+++ client/build/precache-manifest.654d7efb6074ce2bc01a5037c5173b05.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b632761ffc04ecbd9d2a6b4c017bb77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db71d72c2720f819004",
+    "url": "/static/js/2.93c1312f.chunk.js"
+  },
+  {
+    "revision": "ac8f1fb9cbfd57c62b84",
+    "url": "/static/js/main.ec847d75.chunk.js"
+  },
+  {
+    "revision": "cd79c72790072164def0",
+    "url": "/static/js/runtime-main.6ce252c6.js"
+  }
+]);
